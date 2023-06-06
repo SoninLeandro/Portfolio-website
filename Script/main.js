172dd47079f9151c1fb29a6c5be1015743305ca9
Leandro Sonin <soninleandro@gmail.com>
2023-06-06 18:05:17 +0100

    Add functionality

---
+++ Script/main.js
@@ -0,0 +1,10 @@
+function showImage() {
+    let img = document.getElementById('project-img');
+
+    if (img.style.display === 'none') {
+        img.style.display = 'block'
+    } else {
+        img.style.display = 'none'
+    }
+};
+
