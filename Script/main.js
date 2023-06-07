63ab9ef984d4625156b7a807f353d13b11eda23b
Leandro Sonin <soninleandro@gmail.com>
2023-06-07 13:56:43 +0100

    Make it responsive

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,31 @@
-function showImage() {
-    let img = document.getElementById('project-img');
+function showImage1() {
+    let img1 = document.getElementById('project-img 1');
 
-    if (img.style.display === 'none') {
-        img.style.display = 'block'
+    if (img1.style.display === 'none') {
+        img1.style.display = 'block'
     } else {
-        img.style.display = 'none'
+        img1.style.display = 'none'
+    }
+};
+
+function showImage2() {
+    let img2 = document.getElementById('project-img 2');
+
+    if (img2.style.display === 'none') {
+        img2.style.display = 'block'
+    } else {
+        img2.style.display = 'none'
+    }
+};
+
+
+function showImage3() {
+    let img3 = document.getElementById('project-img 3');
+
+    if (img3.style.display === 'none') {
+        img3.style.display = 'block'
+    } else {
+        img3.style.display = 'none'
     }
 };
 
