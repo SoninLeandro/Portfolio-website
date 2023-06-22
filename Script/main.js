bafe1e6035328af16da84bd01877997611065d2b
Leandro Sonin <soninleandro@gmail.com>
2023-06-22 16:27:39 +0100

    Fix Script bug

--- Script/main.js
+++ Script/main.js
@@ -1,40 +1,9 @@
-function showImage1() {
-    let img1 = document.getElementById('project-img 1');
+function showImage(imgID) {
+    let img = document.getElementById(imgID);
 
-    if (img1.style.display === 'none') {
-        img1.style.display = 'block'
+    if (img.style.display === 'none') {
+        img.style.display = 'block'
     } else {
-        img1.style.display = 'none'
-    }
-};
-
-function showImage2() {
-    let img2 = document.getElementById('project-img 2');
-
-    if (img2.style.display === 'none') {
-        img2.style.display = 'block'
-    } else {
-        img2.style.display = 'none'
-    }
-};
-
-
-function showImage3() {
-    let img3 = document.getElementById('project-img 3');
-
-    if (img3.style.display === 'none') {
-        img3.style.display = 'block'
-    } else {
-        img3.style.display = 'none'
-    }
-};
-
-function showImage4() {
-    let img4 = document.getElementById('project-img 4');
-
-    if (img4.style.display === 'none') {
-        img4.style.display = 'block'
-    } else {
-        img4.style.display = 'none'
+        img.style.display = 'none'
     }
 };
